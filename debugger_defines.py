abb6c3a8d6b4406eb3a91f469304738404de08a1
airfan13 <2820655657@qq.com>
2013-09-03 00:13:11 +0800

    fixed

--- debugger_defines.py
+++ debugger_defines.py
@@ -1,41 +1,42 @@
 from ctypes import *
 
-WORD = c_ushort
-DWORD = c_ulong
-LPBYTE = POINTER(c_ubyte)
-LPTSTR = POINTER(c_char)
-HANDLE = c_void_p
+# Let's map the Microsoft types to ctypes for clarity
+WORD	= c_ushort
+DWORD 	= c_ulong
+LPBYTE	= POINTER(c_ubyte)
+LPTSTR	= POINTER(c_char)
+HANDLE	= c_void_p
 
-#Constants
+# Constants
 DEBUG_PROCESS = 0x00000001
 CREATE_NEW_CONSOLE = 0x00000010
 
-#Structures for CreateProcessA()function
+# Structures for CreateProcessA()function
 class STARTUPINFO(Structure):
 	_field_ = [
-		("cb", DWORD),
-		("lpReserved", LPTSTR),
-		("lpDesktop", LPTSTR),
-		("lpTitle", LPTSTR),
-		("dwX", DWORD),
-		("dwY", DWORD),
-		("dwXSize", DWORD),
-		("dwYSize", DWORD),
-		("dwXCountChars", DWORD),
-		("dwYCountChars", DWORD),
+		("cb",				DWORD),
+		("lpReserved", 		LPTSTR),
+		("lpDesktop", 		LPTSTR),
+		("lpTitle", 		LPTSTR),
+		("dwX", 			DWORD),
+		("dwY", 			DWORD),
+		("dwXSize", 		DWORD),
+		("dwYSize", 		DWORD),
+		("dwXCountChars", 	DWORD),
+		("dwYCountChars", 	DWORD),
 		("dwFillAttribute", DWORD),
-		("dwFlags", DWORD),
-		("wShowWindow", WORD),
-		("cbReserved2", WORD),
-		("lpReserved2", LPBYTE),
-		("hStdInput", HANDLE),
-		("hStdOutput", HANDLE),
-		("hStdError", HANDLE),
+		("dwFlags", 		DWORD),
+		("wShowWindow", 	WORD),
+		("cbReserved2", 	WORD),
+		("lpReserved2", 	LPBYTE),
+		("hStdInput", 		HANDLE),
+		("hStdOutput",		HANDLE),
+		("hStdError", 		HANDLE),
 	]
 class PROCESS_INFORMATION(Structure):
 	field = [
-		("hProcess", HANDLE),
-		("hThread", HANDLE),
+		("hProcess",	HANDLE),
+		("hThread", 	HANDLE),
 		("dwProcessId", DWORD),
-		("dwThreadId", DWORD),
+		("dwThreadId", 	DWORD),
 	]
