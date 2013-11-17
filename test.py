4bacdf1c35602765bcbe53fcc1b04fd53dfdd0ec
airfan13 <2820655657@qq.com>
2013-11-18 00:04:37 +0800

    modified

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import debugger
+from debugger_defines import *
 
 debugger = debugger.debugger()
 
@@ -6,33 +7,8 @@ pid = raw_input("Enter the PID of the process to attach to:")
 
 debugger.attach(int(pid))
 
-'''list = debugger.enumerate_threads()
+printf = debugger.func_resolve("msvcrt.dll","printf")
+print "[*] Address of printf: 0x%08x" % printf
 
-# For each thread in the list we want to
-# grad the value of each of the registers
-
-for thread in list:
-	thread_context = debugger.get_thread_context(thread)
-
-	# Now let's output the contents of some of the registers
-	print "[*] Dumping registers for thread ID: 0x%08x" %thread
-	print "[**] EIP: 0x%08x" %thread_context.Eip
-	print "[**] ESP: 0x%08x" %thread_context.Esp
-	print "[**] EBP: 0x%08x" %thread_context.Ebp
-	print "[**] EAX: 0x%08x" %thread_context.Eax
-	print "[**] EBX: 0x%08x" %thread_context.Ebx
-	print "[**] ECX: 0x%08x" %thread_context.Ecx 
-	print "[**] EDX: 0x%08x" %thread_context.Edx
-	print "[*] END DUMP" '''
-
-printf_address = debugger.func_resolve("msvcrt.dll", "printf")
-
-print "[*] Address of printf: 0x%08x" %print_address
-
-debugger.bp_set(printf_address)
-
+debugger.bp_set_hw(printf,1,HW_EXECUTE)
 debugger.run()
-
-'''
-debugger.detach()
-'''
